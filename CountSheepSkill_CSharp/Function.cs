5eccec604a459bc4ae0d38ff1a204f3fc33a13fb
shuhey <shuhey.dev@gmail.com>
2018-12-13 19:47:55 +0900

    Alexaテンプレートを適用。

--- CountSheepSkill_CSharp/Function.cs
+++ CountSheepSkill_CSharp/Function.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,16 +14,247 @@ namespace CountSheepSkill_CSharp
 {
     public class Function
     {
-        
+        //hello
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
-        /// <param name="input"></param>
+        /// <param name="skillRequest"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public SkillResponse FunctionHandler(SkillRequest skillRequest, ILambdaContext context)
         {
-            return input?.ToUpper();
+            SkillResponse skillResponse = null;
+
+            try
+            {
+                //型スイッチの利用
+                switch (skillRequest.Request)
+                {
+                    case LaunchRequest launchRequest:
+                        skillResponse = LaunchRequestHandler(skillRequest);
+                        break;
+                    case IntentRequest intentRequest:
+                        switch (intentRequest.Intent.Name)
+                        {
+                            case "HelloWorldIntent":
+                                skillResponse = HelloWorldIntentHandler(skillRequest);
+                                break;
+                            case "AMAZON.HelpIntent":
+                                skillResponse = HelpIntentHandler(skillRequest);
+                                break;
+                            case "AMAZON.CancelIntent":
+                                skillResponse = CancelAndStopIntentHandler(skillRequest);
+                                break;
+                            case "AMAZON.StopIntent":
+                                skillResponse = CancelAndStopIntentHandler(skillRequest);
+                                break;
+                            default:
+                                //skillResponse = ErrorHandler(skillRequest);
+                                break;
+                        }
+
+                        break;
+                    case SessionEndedRequest sessionEndedRequest:
+                        skillResponse = SessionEndedRequestHandler(skillRequest);
+                        break;
+                    default:
+                        //skillResponse = ErrorHandler(skillRequest);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                skillResponse = ErrorHandler(skillRequest);
+            }
+
+            return skillResponse;
         }
+
+
+
+
+        #region 各インテント、リクエストに対応する処理を担当するメソッドたち
+
+        private SkillResponse LaunchRequestHandler(SkillRequest skillRequest)
+        {
+            var launchRequest = skillRequest.Request as LaunchRequest;
+
+            var speechText = "Welcom to the Alexa Skills Kit, you can say hello!";
+
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            skillResponse.Response.OutputSpeech = new PlainTextOutputSpeech
+            {
+                Text = speechText
+            };
+            skillResponse.Response.Reprompt = new Reprompt
+            {
+                OutputSpeech = new PlainTextOutputSpeech
+                {
+                    Text = speechText
+                }
+            };
+            skillResponse.Response.Card = new SimpleCard
+            {
+                Title = "Hello World",
+                Content = speechText
+            };
+
+            return skillResponse;
+        }
+
+        //ご自分で追加したインテントに合わせて名前や処理を変更してください。
+        private SkillResponse HelloWorldIntentHandler(SkillRequest skillRequest)
+        {
+            var intentRequest = skillRequest.Request as IntentRequest;
+
+            var speechText = "Hello World!";
+
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            skillResponse.Response.OutputSpeech = new PlainTextOutputSpeech
+            {
+                Text = speechText
+            };
+            skillResponse.Response.Card = new SimpleCard
+            {
+                Title = "Hello World",
+                Content = speechText
+            };
+            skillResponse.Response.ShouldEndSession = true;
+
+            return skillResponse;
+        }
+
+        /// <summary>
+        /// 組み込みインテント用
+        /// </summary>
+        /// <param name="skillRequest"></param>
+        /// <returns></returns>
+        private SkillResponse HelpIntentHandler(SkillRequest skillRequest)
+        {
+            var intentRequest = skillRequest.Request as IntentRequest;
+
+            var speechText = "You can say hello to me!";
+
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            skillResponse.Response.OutputSpeech = new PlainTextOutputSpeech
+            {
+                Text = speechText
+            };
+            skillResponse.Response.Reprompt = new Reprompt
+            {
+                OutputSpeech = new PlainTextOutputSpeech
+                {
+                    Text = speechText
+                }
+            };
+            skillResponse.Response.Card = new SimpleCard
+            {
+                Title = "Hello World",
+                Content = speechText
+            };
+
+            return skillResponse;
+        }
+
+
+        /// <summary>
+        /// 組み込みインテント用
+        /// </summary>
+        /// <param name="skillRequest"></param>
+        /// <returns></returns>
+        private SkillResponse CancelAndStopIntentHandler(SkillRequest skillRequest)
+        {
+            var intentRequest = skillRequest.Request as IntentRequest;
+
+            var speechText = "Goodbye!";
+
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            skillResponse.Response.OutputSpeech = new PlainTextOutputSpeech
+            {
+                Text = speechText
+            };
+            skillResponse.Response.Card = new SimpleCard
+            {
+                Title = "Hello World",
+                Content = speechText
+            };
+            skillResponse.Response.ShouldEndSession = true;
+
+            return skillResponse;
+        }
+
+        /// <summary>
+        /// 組み込みインテント用
+        /// </summary>
+        /// <param name="skillRequest"></param>
+        /// <returns></returns>
+        private SkillResponse SessionEndedRequestHandler(SkillRequest skillRequest)
+        {
+            var sesstionEndedRequest = skillRequest.Request as SessionEndedRequest;
+
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            skillResponse.Response.ShouldEndSession = true;
+
+            return skillResponse;
+        }
+
+        /// <summary>
+        /// 組み込みインテント用
+        /// </summary>
+        /// <param name="skillRequest"></param>
+        /// <returns></returns>
+        private SkillResponse ErrorHandler(SkillRequest skillRequest)
+        {
+            var speechText = "Sorry, I can't understand the command. Please say again.";
+
+            var skillResponse = new SkillResponse
+            {
+                Version = "1.0",
+                Response = new ResponseBody()
+            };
+
+            skillResponse.Response.OutputSpeech = new PlainTextOutputSpeech
+            {
+                Text = speechText
+            };
+            skillResponse.Response.Reprompt = new Reprompt
+            {
+                OutputSpeech = new PlainTextOutputSpeech
+                {
+                    Text = speechText
+                }
+            };
+            skillResponse.Response.ShouldEndSession = true;//エラーなのでセッションを終了させなければならない。
+
+            return skillResponse;
+        }
+
+        #endregion
+
     }
 }
